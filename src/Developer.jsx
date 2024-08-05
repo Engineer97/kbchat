046b26da3e8f8fbc8cb46ce06d22844a1aeb3e75
Engineer97 <dbgmlwo1997@naver.com>
2024-08-05 16:57:10 +0900

    kb챗 첫번째 커밋

---
+++ src/Developer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Developer extends React.Component {
+    render() {
+        return(
+            <div>
+                <button>개발자용 사이트</button>
+            </div>
+        )
+    }
+}
+
+export default Developer;
---
+++ src/Hangone.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Hangone extends React.Component {
+    render() {
+        return(
+            <div>
+                <button>행원용 사이트</button>
+            </div>
+        )
+    }
+}
+
+export default Hangone;
